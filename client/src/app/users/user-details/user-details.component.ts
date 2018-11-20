4cb02044181692fd911ab4ebfd44e6fef07b6983
terhoor <terhoor@yandex.ru>
2018-11-20 16:09:57 +0400

    ///

--- client/src/app/users/user-details/user-details.component.ts
+++ client/src/app/users/user-details/user-details.component.ts
@@ -16,7 +16,7 @@ export class UserDetailsComponent implements OnInit {
   user: IUser;
   subj: string[];
   displayedColumns: string[];
-
+  displayedColumnsUser: string[];
 
   constructor(
     private route: ActivatedRoute,
@@ -25,6 +25,7 @@ export class UserDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.getUser();
+    this.displayedColumnsUser = ['lastName', 'firstName', 'patronymic', 'role'];
   }
 
   getUser(): void {
@@ -32,7 +33,6 @@ export class UserDetailsComponent implements OnInit {
     this.usersService.getById(id)
       .subscribe((user) => {
         this.user = user;
-
         if (user.roles.indexOf('Student') !== -1) {
           this.subj = Object.keys(this.user.subjects);
         } else if (user.roles.indexOf('Teacher') !== -1) {
