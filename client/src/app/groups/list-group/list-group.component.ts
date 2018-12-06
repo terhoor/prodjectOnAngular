995407bc0867e992789f808997c23b27a7062e9b
terhoor <terhoor@yandex.ru>
2018-12-06 13:53:53 +0400

    add not work sort for list-group

--- client/src/app/groups/list-group/list-group.component.ts
+++ client/src/app/groups/list-group/list-group.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { UsersService } from 'src/app/shared/services/users.service';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-group',
@@ -7,21 +9,23 @@ import { UsersService } from 'src/app/shared/services/users.service';
   styleUrls: ['./list-group.component.css']
 })
 export class ListGroupComponent implements OnInit, OnDestroy {
-  groups: string[] = [];
-  groups$;
+  groups: MatTableDataSource<string>;
+  groups$: Subscription;
+  @ViewChild(MatSort) sort: MatSort;
   displayedColumns: string[] = ['groups'];
   constructor(private usersService: UsersService) { }
 
   ngOnInit() {
     this.groups$ = this.usersService.groups.subscribe((value) => {
-      this.groups = value;
+      this.groups = new MatTableDataSource(value);
+      this.groups.sort = this.sort;
     });
   }
 
   ngOnDestroy() {
-    if (this.groups$) {
-      this.groups$.unsubscribe();
-    }
+    // if (this.groups$) {
+    //   this.groups$.unsubscribe();
+    // }
   }
 
 }
